087e9b39e600b11c9ab2d02f95e73529101b8f8c
PhoenixItachi <juventus09deva@yahoo.com>
2017-01-22 21:19:53 +0200

    Page merged. New Index. Documents icon added.

---
+++ ProiectColectiv.Web/ViewModel/DocumentAndTasksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectColectiv.Web.ViewModel
+{
+    public class DocumentAndTasksViewModel
+    {
+        public List<DocumentDetailViewModel> Documents { get; set; }
+        public List<DocumentTaskViewModel> Tasks { get; set; }
+    }
+}
